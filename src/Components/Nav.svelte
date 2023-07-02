547a8b5b45fdba2c566fb6383185806ab2009524
Ahmed Onour <ahmedonour.49@gmail.com>
2023-07-02 06:11:51 +0200

    init

---
+++ src/Components/Nav.svelte
@@ -0,0 +1,14 @@
+<nav>
+    <div class="logo">
+        <h1>as</h1>
+    </div>
+    <div class="nav-btn">
+        <div class="icons">
+
+        </div>
+        <p>Let's Talk</p>
+    </div>
+</nav>
+<style>
+    
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Nav from "../Components/Nav.svelte";
+
+</script>
+<main>
+    <Nav />
+</main>
+<style>
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    main{
+        display: grid;
+        place-items: center;
+    }
+    h1{
+        color: black;
+    }
+</style>
